a197f612c0f979324fdfa5183d6899b96238200e
Mike Limberg <limbergmike@gmail.com>
2017-08-28 13:53:59 -0600

    Add dolphins

---
+++ src/data/dolphin-data.js
@@ -0,0 +1,42 @@
+import img1 from './dolphin-images/dolphin-1.png'
+import img2 from './dolphin-images/dolphin-2.jpg'
+import img3 from './dolphin-images/dolphin-3.jpg'
+import img4 from './dolphin-images/dolphin-4.jpg'
+import img5 from './dolphin-images/dolphin-5.jpg'
+import img6 from './dolphin-images/dolphin-6.jpg'
+import img7 from './dolphin-images/dolphin-7.jpg'
+import img8 from './dolphin-images/dolphin-8.jpg'
+import img9 from './dolphin-images/dolphin-9.jpg'
+
+const bio1 = "I will pack your sweet pink mouth with so much ice cream you'll be the envy of every Jerry and Jane on the block! A trick is something a whore does for money… or cocaine. I'll have a vodka rocks. (Mom, it's breakfast time.) And a piece of toast. No, I was ashamed to be SEEN with you. I like being with you. What do you expect, Mother? I'm half machine! I'm a monster!! Wine only turns to alcohol if you let it sit."
+
+const bio2 = "We have unlimited juice? This party is going to be off the hook. You stay on top of her Buddy. Don't be afraid to ride her. Hard. God knows they're squinters. I know what an erection feels like, Michael. No, it's the opposite. It's like my heart is getting hard. Did Ted make an appointment? No. Well then Ted can GET THE HELL OUT OF THIS OFFICE! YOU GET THE HELL OUT! Even though so many people in this office are begging for it. Turns out he ended up getting too friendly with the teddy bear."
+
+const bio3 = "However, she mistook the drowsy eye alcohol warning for a winking eye alcohol suggestion. What do you expect, Mother? I'm half machine! I'm a monster!!"
+
+
+const bio4 = "He's going to be all right. Also, your knee is on my heart. So you take your mom to work every day? Bummer. Moms are such a pain in the ass, huh? It's, like, die already! Well, I spent so much time making sweet love on my wife that it's hard to hear anything over the clatter of her breasts. I shall hide behind the couch. (Guy's a pro.) I don't criticize you! And if you're worried about criticism, sometimes a diet is the best defense. Aren't you the sweetest thing, spending time with what's left of your uncle. It's one banana Michael, what could it cost, ten dollars?"
+
+const bio5 = "A trick is something a whore does for money… or cocaine. I know what an erection feels like, Michael. A flower in my garden, a mystery in my panties. That's my son, you pothead! Yeah, well, have you seen the new Mustang? You could hump that hood. A-coodle-doodle-doo. A-coodle-doodle-doo. And with deep, deep concentration and, and great focus, he's often able to achieve an erect–"
+
+const bio6 = "I'm sure Egg is a great person. In fact, it was a box of Oscar's legally obtained medical marijuana. Primo bud. Real sticky weed. You go buy a tape recorder and record yourself for a whole day. I think you'll be surprised at some of your phrasing. I'm in Vegas this week and would like to point out the Blue Man Group is *actually* hiring. The support group? I've used one adjective to describe myself. What is it? I'll buy you a hundred George Michaels that you can teach to drive! 'Circumvent'. It means 'to go around.' Gob: The old 'reach around.' If you didn't have adult onset diabetes, I wouldn't mind giving you a little sugar."
+
+const bio7 = "Look at us, crying like a bunch of girls on the last day of camp. I'm tired of trying to find happiness through lies and self-medicating. If you need me, I'll be at the bar. Maybe it was the eleven months he spent in the womb. The doctor said there were claw marks on the walls of her uterus. She's a contestant. It's sorta like an inner beauty pageant. Ah, there it is. We all need to pick a day to try and make trend. I spent so much time making sweet love on my wife that it's hard to hear anything over the clatter of her breasts."
+
+const bio8 = "No, no, it's pronounced a-nal-ra-pist. And here you are coming out of your mother's third base! I may have committed some light treason. WHY was this show cancelled? I mean, COME ON.'"
+
+const bio9 = "WHY was this show cancelled? I mean, COME ON. This is not me encouraging you to go here and write a review. No."
+
+const dolphinData = [
+  { id: 1, name: 'The Hammer', image: img1, type: 'dolphins', bio: bio1 },
+  { id: 2, name: 'Carol', image: img2, type: 'dolphins', bio: bio2  },
+  { id: 3, name: 'Jaws', image: img9, type: 'dolphins', bio: bio3 },
+  { id: 4, name: 'The Bash Bros', image: img4, type: 'dolphins', bio: bio4 },
+  { id: 5, name: 'Spot', image: img5, type: 'dolphins', bio: bio5 },
+  { id: 6, name: 'Kid Cutty', image: img6, type: 'dolphins', bio: bio6 },
+  { id: 7, name: 'Dory', image: img7, type: 'dolphins', bio: bio7 },
+  { id: 8, name: 'Darrell', image: img8, type: 'dolphins', bio: bio8 },
+  { id: 9, name: 'Bernie', image: img3, type: 'dolphins', bio: bio9 }
+]
+
+export default dolphinData
