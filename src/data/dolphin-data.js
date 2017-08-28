8d73b6a58a4eee09c274b2838b13a054011b6229
Mike Limberg <limbergmike@gmail.com>
2017-08-28 14:02:22 -0600

    Fix dolphin data

--- src/data/dolphin-data.js
+++ src/data/dolphin-data.js
@@ -1,4 +1,4 @@
-import img1 from './dolphin-images/dolphin-1.png'
+import img1 from './dolphin-images/dolphin-1.jpg'
 import img2 from './dolphin-images/dolphin-2.jpg'
 import img3 from './dolphin-images/dolphin-3.jpg'
 import img4 from './dolphin-images/dolphin-4.jpg'
@@ -30,13 +30,13 @@ const bio9 = "WHY was this show cancelled? I mean, COME ON. This is not me encou
 const dolphinData = [
   { id: 1, name: 'Greg', image: img1, type: 'dolphins', bio: bio1 },
   { id: 2, name: 'Betty', image: img2, type: 'dolphins', bio: bio2  },
-  { id: 3, name: 'DJ Kool', image: img9, type: 'dolphins', bio: bio3 },
+  { id: 3, name: 'DJ Kool', image: img3, type: 'dolphins', bio: bio3 },
   { id: 4, name: 'Sir Smiles', image: img4, type: 'dolphins', bio: bio4 },
   { id: 5, name: 'Geoffery', image: img5, type: 'dolphins', bio: bio5 },
   { id: 6, name: 'Princess Grenadine', image: img6, type: 'dolphins', bio: bio6 },
   { id: 7, name: 'Dan Marino', image: img7, type: 'dolphins', bio: bio7 },
   { id: 8, name: 'Pinnochio', image: img8, type: 'dolphins', bio: bio8 },
-  { id: 9, name: 'Pinky', image: img3, type: 'dolphins', bio: bio9 }
+  { id: 9, name: 'Pinky', image: img9, type: 'dolphins', bio: bio9 }
 ]
 
 export default dolphinData
